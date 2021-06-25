2900bbe19f655aa52c9ac89c311ee4aeb3708913
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-06-25 18:20:42 +0900

    Optional

--- jdk-api/src/com/kh/jdk8/optional/OptionalStudy.java
+++ jdk-api/src/com/kh/jdk8/optional/OptionalStudy.java
@@ -1,19 +1,103 @@
 package com.kh.jdk8.optional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * OPtional
+ * 
+ * Optional
+ * - 존재할 수도 있고, 안할수도 있는 객체를 감싸고 있는 wrapper객체
+ * - 변수명에 maybeMember, optUser와 같이 Optional타입을 암묵적으로 제시한다.
+ * 
+ * 1. Optional.empty() : 비어있는 Optional객체
+ * 2. Optional.of(obj) : 결코 null이 아닌 객체를 담은 Optional객체
+ * 3. Optional.ofNullable(obj) : null일수도 있는 Optional객체
+ * 
+ * - get()
+ * - orElse(T)
+ * - orElseGet(Supplier)
+ * - orElseThrow(Supplier)
+ * - isPresent:boolean
+ * - ifPresent(Consumer)
  * 
  * if(s != null)
- * s.foo()
+ * 		s.foo()
+ *
  */
 public class OptionalStudy {
+
 	public static void main(String[] args) {
 		OptionalStudy study = new OptionalStudy();
-		study.test1();
+//		study.test1();
+		study.test2();
 	}
-
-	private void test1() {
-		// TODO Auto-generated method stub
+	
+	public void test2() {
+		Member member = new Member();
+		member.setEmail("honggd@naver.com");
+		Order order = new Order();
+		order.setMember(member);
+		System.out.println(getEmailOfMemberFromOrder(order));
+		
+		System.out.println(getEmailOfMemberFromOrder(null)); 
+		System.out.println(getEmailOfMemberFromOrder(new Order())); 
+	}
+	
+	public String getEmailOfMemberFromOrder(Order order) {
+		return Optional.ofNullable(order)
+				.map(Order::getMember)
+				.map(Member::getEmail)
+				.orElse("메일이 존재하지 않습니다.");
+		
+//		return Optional.ofNullable(order)
+//					   .map(o -> o.getMember())
+//					   .map(member -> member.getEmail())
+//					   .orElse("메일이 존재하지 않습니다.");
+		
+//		if(order != null) {
+//			Member member = order.getMember();
+//			if(member != null) {
+//				String email = member.getEmail();
+//				return email;
+//			}
+//		}
+//		return "메일이 존재하지 않습니다.";
+//		return order.getMember().getEmail();
+	}
+	
+	@Data
+	@NoArgsConstructor
+	static class Order {
+		Member member;
+		int count;
+	}
+	@Data
+	@NoArgsConstructor
+	static class Member {
+		String id;
+		String email;
+	}
+	
+	public void test1() {
+		List<String> list = Arrays.asList("홍길동", "신사", null, "");
+		list.stream()
+			.forEach(s -> System.out.println(s + " : " + getStringLength(s)));
 		
 	}
+
+	private int getStringLength(String str) {
+		Optional<String> maybeStr = Optional.ofNullable(str);
+		return maybeStr
+					.map(s -> s.length())
+//					.orElse(0);
+					.orElseThrow(() -> new RuntimeException(str));
+	}
 }
+
+
+
+
